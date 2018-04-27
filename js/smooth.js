266dc37ae48a2d2af062c0c2f95277f78aa10f3b
Alisson Lauffer <alissonvitortc@gmail.com>
2018-04-27 19:43:34 -0300

    script para navegação suave

---
+++ js/smooth.js
@@ -0,0 +1,9 @@
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+
+    return false;
+});
