f60bcad56076935cd9d021af78e43ef6f6f1fc91
Alisson Lauffer <alissonvitortc@gmail.com>
2018-04-28 02:32:49 -0300

    Strings must use doublequote.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
 (function($){
   $(function(){
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
