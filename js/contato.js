e000153f132feb34ef7c6c57b3d2b9e5f6d0bd15
Alisson Lauffer <alissonvitortc@gmail.com>
2018-08-08 18:03:26 -0300

    Update contato.js

--- js/contato.js
+++ js/contato.js
@@ -10,7 +10,7 @@ $("#sendMessage").click(function(event) {
         text: $('#text').val()
       },
       success: function(data) {
-        $('#sendMessage').write(data);
+        document.getElementById('#sendMessage').innerHTML=data;
       }
     }
   )
