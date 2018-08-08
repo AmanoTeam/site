7d372b554c09ab9685de8cf373afc180b01f3575
Alisson Lauffer <alissonvitortc@gmail.com>
2018-08-08 19:39:43 -0300

    Update contato.js

--- js/contato.js
+++ js/contato.js
@@ -1,18 +1,19 @@
 $("#sendMessage").click(function(event) {
-  $("#sendMessage").replaceWith("<p id='sendMessage'>Enviando...</p>");
-  $.ajax(
-    {
-      type: "POST",
-      url: "https://amanosite.000webhostapp.com/contact/",
-      data: {
-        name: $("#nome").val(),
-        username: $("#username").val(),
-        assunto: $("#assunto").val(),
-        text: $("#texto").val(),
-      },
-      success: function(data) {
-        $("#sendMessage").replaceWith(data);
+    $("#sendMessage").replaceWith("<a id='sendMessage'>Enviando...</a>");
+    $.ajax(
+      {
+        type: "POST",
+        url: "https://amanosite.000webhostapp.com/contact/",
+        data: {
+          name: $("#nome").val(),
+          username: $("#username").val(),
+          assunto: $("#assunto").val(),
+          text: $("#texto").val(),
+        },
+        success: function(data) {
+          $("#sendMessage").replaceWith(data);
+        }
       }
-    }
-  )
-})
+    );
+  }
+);
