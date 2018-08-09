62bb80ab2090d61fd390d1a16f518bb4a16f6077
Alisson Lauffer <alissonvitortc@gmail.com>
2018-08-09 01:21:34 -0300

    Updates

--- js/contato.js
+++ js/contato.js
@@ -1,19 +1,28 @@
+var $button = "<a class='btn blue darken-2 waves-effect waves-light' name='contatoBtn' id='sendMessage'>Enviar</a>"
+
 $("#sendMessage").click(function(event) {
-    $("#sendMessage").replaceWith("<a class='white-text' id='sendMessage'>Enviando...</a>");
-    $.ajax(
-      {
-        type: "POST",
-        url: "https://amanosite.000webhostapp.com/contact/",
-        data: {
-          name: $("#name").val(),
-          username: $("#username").val(),
-          assunto: $("#assunto").val(),
-          text: $("#texto").val(),
-        },
-        success: function(data) {
-          $("#sendMessage").replaceWith(data);
+  if ($("#name").val() == "") {
+    swal("Preencha seu nome", "Por favor preencha o seu nome.", "error")
+  } else if ($("#texto").val() == "") {
+    swal("Escreva sua mensagem", "Por favor escreva sua mensagem.", "error")
+  } else {
+      $("#sendMessage").replaceWith("<a class='white-text' id='sendMessage'>Enviando...</a>");
+      $.ajax(
+        {
+          type: "POST",
+          url: "https://amanosite.000webhostapp.com/contact/",
+          data: {
+            name: $("#name").val(),
+            username: $("#username").val(),
+            assunto: $("#assunto").val(),
+            text: $("#texto").val(),
+          },
+          success: function(data) {
+            $("#sendMessage").replaceWith(data);
+            swal(data, "A sua mensagem foi enviada para a nossa equipe!", "success")
+          }
         }
-      }
-    );
+      );
+    }
   }
 );
