4e8cce0425c009a04daeafb0fd3be7060036560d
Alisson Lauffer <alissonvitortc@gmail.com>
2019-06-07 20:59:57 -0300

    Some changes for contact us section

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,26 @@
-$("#texto").keyup(function(event) {
+$("#message-input").keyup(function(event) {
+  $("#message-label").removeClass('orange-text red-text');
   if ($(this).val().length > 512) {
     $(this).val($(this).val().substring(0, 512));
   }
 });
 
+$("#name-input").keyup(function(event) {
+  $("#name-label").removeClass('red-text');
+});
+
 $("#submit").click(function(event) {
-  if ($("#name").val() === "") {
-    swal("Preencha seu nome", "Por favor preencha o seu nome.", "error");
-  } else if ($("#texto").val() === "") {
-    swal("Escreva sua mensagem", "Por favor escreva sua mensagem.", "error");
+  if ($("#name-input").val() === "") {
+    document.getElementById('submitted').innerHTML = "<b>Please fill in your name.</b>";
+    $("#name-label").addClass('red-text');
+  } else if ($("#message-input").val() === "") {
+    document.getElementById('submitted').innerHTML = "<b>Please type your message.</b>";
+    $("#message-label").removeClass('orange-text');
+    $("#message-label").addClass('red-text');
+  } else if ($("#message-input").val().length < 25) {
+    document.getElementById('submitted').innerHTML = "<b>Your message can't be shorter than 25 characters.</b>";
+    $("#message-label").removeClass('red-text');
+    $("#message-label").addClass('orange-text');
   } else {
       document.getElementById('submitted').innerHTML = "sending...";
       $.ajax(
