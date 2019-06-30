a4df1b13126ca3ce42b10c9cb603d0fbbcc0ed9d
Alisson Lauffer <alissonvitortc@gmail.com>
2019-06-30 19:03:24 -0300

    Better project modals; Add loading spinner

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+// Set up modals
+$("#amanobot-a").animatedModal({color: "#0C1020", animatedIn: "zoomIn", animatedOut: "fadeOut"});
+$("#brdotml-a").animatedModal({color: "#111", animatedIn: "zoomIn", animatedOut: "fadeOut"});
+$("#duckpy-a").animatedModal({color: "#DA4A26", animatedIn: "zoomIn", animatedOut: "fadeOut"});
+$("#eduurobot-a").animatedModal({color: "#C90000", animatedIn: "zoomIn", animatedOut: "fadeOut"});
+$("#lyricspy-a").animatedModal({color: "#000", animatedIn: "zoomIn", animatedOut: "fadeOut"});
+$("#passwordrobot-a").animatedModal({color: "#355BCF", animatedIn: "zoomIn", animatedOut: "fadeOut"});
+$("#tgbackup-a").animatedModal({color: "#31A8F6", animatedIn: "zoomIn", animatedOut: "fadeOut"});
+$("#userlixo-a").animatedModal({color: "#8C5AD3", animatedIn: "zoomIn", animatedOut: "fadeOut"});
+
+$( document ).ready(function() {
+  $('#loading').hide('slow');
+});
+
 (function($) {
   "use strict"; // Start of use strict
 
