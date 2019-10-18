e5f05111aa06e0d9111dbfa8426b4847e009ae1d
Alisson Lauffer <alissonvitortc@gmail.com>
2019-10-17 21:07:07 -0300

    Use browser form behavior instead of hardcoding it with JavaScript (and reformat code)

--- js/contact.js
+++ js/contact.js
@@ -9,35 +9,26 @@ $("#name-input").keyup(function(event) {
   $("#name-label").removeClass("red-text");
 });
 
-$("#submit").click(function(event) {
-  if ($("#name-input").val() === "") {
-    document.getElementById("submitted").innerHTML = "<b>Please fill in your name.</b>";
-    $("#name-label").addClass("red-text");
-  } else if ($("#message-input").val() === "") {
-    document.getElementById("submitted").innerHTML = "<b>Please type your message.</b>";
-    $("#message-label").removeClass("orange-text");
-    $("#message-label").addClass("red-text");
-  } else if ($("#message-input").val().length < 25) {
+$("#contact-form").submit(function(event) {
+  event.preventDefault();
+  if ($("#message-input").val().length < 25) {
     document.getElementById("submitted").innerHTML = "<b>Your message can't be shorter than 25 characters.</b>";
     $("#message-label").removeClass("red-text");
     $("#message-label").addClass("orange-text");
   } else {
-      document.getElementById("submitted").firstChild.nodeValue = "sending...";
-      $.ajax(
-        {
-          type: "POST",
-          url: "https://api.amanoteam.ml/sendcontact/",
-          data: {
-            name: $("#name-input").val(),
-            username: $("#username-input").val(),
-            subject: $("#subject-input").val(),
-            text: $("#message-input").val(),
-          },
-          success: function(data) {
-            document.getElementById("submitted").firstChild.nodeValue = data;
-          }
-        }
-      );
-    }
+    document.getElementById("submitted").firstChild.nodeValue = "sending...";
+    $.ajax({
+      type: "POST",
+      url: "https://api.amanoteam.ml/sendcontact/",
+      data: {
+        name: $("#name-input").val(),
+        username: $("#username-input").val(),
+        subject: $("#subject-input").val(),
+        text: $("#message-input").val(),
+      },
+      success: function(data) {
+        document.getElementById("submitted").firstChild.nodeValue = data;
+      }
+    });
   }
-);
+});
