4843cabe47376c63399faa65ab4e1a9b2cc04b93
Alisson Lauffer <alissonvitortc@gmail.com>
2020-01-16 00:35:29 -0300

    Add customgram project and make some changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Set up modals
 $("#amanobot-a").animatedModal({ color: "#0C1020", animatedIn: "zoomIn", animatedOut: "fadeOut" });
 $("#brdotml-a").animatedModal({ color: "#111", animatedIn: "zoomIn", animatedOut: "fadeOut" });
+$("#customgram-a").animatedModal({ color: "#3C7DC2", animatedIn: "zoomIn", animatedOut: "fadeOut" });
 $("#duckpy-a").animatedModal({ color: "#DA4A26", animatedIn: "zoomIn", animatedOut: "fadeOut" });
 $("#eduurobot-a").animatedModal({ color: "#C90000", animatedIn: "zoomIn", animatedOut: "fadeOut" });
 $("#lyricspy-a").animatedModal({ color: "#000", animatedIn: "zoomIn", animatedOut: "fadeOut" });
