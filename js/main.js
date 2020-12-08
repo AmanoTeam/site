c34885a89896395fdea925c4de1cb21163341bf6
Alisson Lauffer <alissonvitortc@gmail.com>
2020-12-08 04:18:04 -0300

    More codacy fixes

--- js/main.js
+++ js/main.js
@@ -50,6 +50,6 @@ navbarCollapse();
 // Collapse the navbar when page is scrolled
 window.addEventListener("scroll", navbarCollapse);
 
-particlesJS.load('main-header-bg', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+particlesJS.load("main-header-bg", "assets/particles.json", function() {
+  console.log("callback - particles.js config loaded");
 });
