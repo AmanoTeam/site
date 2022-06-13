bbe77d360d068478e41e805d706928994f957caf
Alisson Lauffer <alissonvitortc@gmail.com>
2022-06-13 02:46:30 -0300

    tsparticles 1.39.1 -> 2.0.6

--- js/main.js
+++ js/main.js
@@ -92,7 +92,61 @@ navbarCollapse();
 // Collapse the navbar when page is scrolled
 window.addEventListener("scroll", navbarCollapse);
 
-particlesJS.load("main-header-bg", "assets/particles.json", () => {});
+tsParticles.load("main-header-bg", {
+  background: {
+    color: {
+      value: "#232741"
+    }
+  },
+  particles: {
+    color: {
+      value: "#ffffff"
+    },
+    move: {
+      enable: true,
+      outModes: {
+        bottom: "out",
+        left: "out",
+        right: "out",
+        top: "out"
+      },
+      random: true,
+      speed: 1
+    },
+    number: {
+      density: {
+        enable: true
+      },
+      value: 160
+    },
+    opacity: {
+      random: {
+        enable: true
+      },
+      value: {
+        min: 0,
+        max: 1
+      },
+      animation: {
+        enable: true,
+        speed: 1
+      }
+    },
+    size: {
+      random: {
+        enable: true
+      },
+      value: {
+        min: 1,
+        max: 3
+      },
+      animation: {
+        speed: 4,
+        minimumValue: 0.3
+      }
+    }
+  }
+});
 
 
 let copyYear = document.getElementById("copy-year");
