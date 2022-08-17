7fe0123bdc15dd8d8c14a11d20c2cc4cfef4f5a9
Alisson Lauffer <alissonvitortc@gmail.com>
2022-08-17 03:39:41 -0300

    drop Material Icons
    there were just 2 icons being used

--- src/faIcons.js
+++ src/faIcons.js
@@ -1,8 +1,8 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faBook, faGlobe, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faChevronDown, faGlobe, faNewspaper, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faGitlab, faGooglePlay, faPython, faTelegram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faBook, faGlobe, faNewspaper);
+library.add(faBook, faChevronDown, faGlobe, faNewspaper, faXmark);
 library.add(faGithub, faGitlab, faGooglePlay, faPython, faTelegram, faTwitter);
 
 dom.watch();
