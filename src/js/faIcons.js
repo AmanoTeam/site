de793bf094fce5616f27b873c82f866caab3734a
Alisson Lauffer <alissonvitortc@gmail.com>
2023-08-22 23:08:31 -0300

    chore: bump dependencies and drop X/Twitter hyperlink
    Amano's X/Twitter account has been suspended a long time ago, and I got no response from their (non-existent) support.

--- src/js/faIcons.js
+++ src/js/faIcons.js
@@ -1,8 +1,8 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faBars, faBook, faChevronDown, faGlobe, faNewspaper, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faGitlab, faGooglePlay, faPython, faTelegram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faGitlab, faGooglePlay, faPython, faTelegram } from "@fortawesome/free-brands-svg-icons";
 
 library.add(faBars, faBook, faChevronDown, faGlobe, faNewspaper, faXmark);
-library.add(faGithub, faGitlab, faGooglePlay, faPython, faTelegram, faTwitter);
+library.add(faGithub, faGitlab, faGooglePlay, faPython, faTelegram);
 
 dom.watch();
