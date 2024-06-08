09ac57bfafd07380a49873cd37187efbf8c4a485
Alisson Lauffer <alissonvitortc@gmail.com>
2024-06-08 16:31:46 -0300

    add astro-purgecss to project
    it helped reduce around 84 KB of CSS

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [purgecss()],
   output: "static"
 });
