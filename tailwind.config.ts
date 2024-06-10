e6e7764995e1be46ea8f31d8261b7d20d518e90b
Alisson Lauffer <alissonvitortc@gmail.com>
2024-06-10 19:55:54 -0300

    move js configs into ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,16 @@
+import { Config } from 'tailwindcss';
 import daisyui from "daisyui";
 
-/** @type {import('tailwindcss').Config} */
-export default {
+const config: Config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif"]
+        sans: ["Roboto", "sans-serif"],
       },
     },
   },
   plugins: [daisyui],
 };
+
+export default config;
