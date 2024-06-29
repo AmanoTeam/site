ad4b596a070c3443c72c6b24415c9e1a08000f65
Alisson Lauffer <alissonvitortc@gmail.com>
2024-06-29 20:03:27 -0300

    drop purgecss
    it's not that useful anymore, since we're using Tailwind CSS now

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import purgecss from "astro-purgecss";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), purgecss()],
+  integrations: [tailwind()],
   output: "static"
 });
