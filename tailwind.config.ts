1c0b0460cfebc2be348e44fc8241e9589013e27f
Alisson Lauffer <alissonvitortc@gmail.com>
2024-07-09 23:33:25 -0300

    update deps and move fonts to variable weight

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif"],
+        sans: ["Roboto Flex Variable", "sans-serif"],
       },
     },
   },
